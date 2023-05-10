6b8ba38efe16a5f4ab704ccd4b884d380fcd82e8
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-05-10 23:50:54 +0300

    pipelines_db

--- ppl_population/ppl_population/pipelines.py
+++ ppl_population/ppl_population/pipelines.py
@@ -6,8 +6,37 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import logging
+import sqlite3
 
 
-class PplPopulationPipeline:
+class SQLite3PPLPipeline:
+    def open_spider(self, spider):
+        self.connection = sqlite3.connect('transcripts.db')
+        self.c = self.connection.cursor()
+        try:
+            self.c.execute('''  
+                CREATE TABLE transcripts(
+                country TEXT,
+                year TEXT,
+                population TEXT
+                )
+            ''')
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            pass
+
+
+    def close_spider(self, spider):
+        self.connection.close()
+    
     def process_item(self, item, spider):
+        self.c.execute('''
+            INSERT INTO transcripts (country,year,population) VALUES(?,?,?)
+        ''',(
+            item.get('country'),
+            item.get('year'),
+            item.get('population'),
+        ))
+        self.connection.commit()
         return item
--- ppl_population/ppl_population/settings.py
+++ ppl_population/ppl_population/settings.py
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "ppl_population.pipelines.PplPopulationPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "ppl_population.pipelines.SQLite3PPLPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
