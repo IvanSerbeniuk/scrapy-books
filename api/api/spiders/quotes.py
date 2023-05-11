09e639e4239bb74a9e0b31ac3c2bb218e1411419
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-05-11 13:01:54 +0300

    id

---
+++ api/api/spiders/quotes.py
@@ -0,0 +1,22 @@
+import scrapy
+# To convert json to dictionary
+import json 
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/api/quotes?page=1"]
+
+    def parse(self, response):
+        json_response = json.loads(response.body)
+        quotes = json_response.get('quotes')
+        for quote in quotes:
+            yield{
+                'author': quote.get('author').get('name'),
+                'tags': quote.get('tags'),
+                'text': quote.get('text'),
+            }
+        has_next = json_response.get('has_next')
+        if has_next:
+            next_page_nember = json_response.get('page') + 1
+            yield scrapy.Request(url=f'https://quotes.toscrape.com/api/quotes?page={next_page_nember}', callback=self.parse)
