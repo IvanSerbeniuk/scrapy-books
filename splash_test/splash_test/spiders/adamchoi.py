0e15504fb8051fcc497bc25a901f9174c8e8524a
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-05-19 02:04:11 +0300

    test works

--- splash_test/splash_test/spiders/adamchoi.py
+++ splash_test/splash_test/spiders/adamchoi.py
@@ -5,22 +5,23 @@ from scrapy_splash import SplashRequest
 class AdamchoiSpider(scrapy.Spider):
     name = "adamchoi"
     allowed_domains = ["www.adamchoi.co.uk"]
-    start_urls = ["http://www.adamchoi.co.uk/"]
+    # start_urls = ["http://www.adamchoi.co.uk/"]
 
-    scriipt = '''
+    script = '''
         function main(splash, args)
-            splash.private_mode_enabled = false
-            assert(splash:go(args.url))
+            assert(splasLh:go(args.url))
+            assert(splash:wait(2))
             all_matches = assert(splash:select_all('label.btn.btn-sm.btn-primary'))
             all_matches[2]:mouse_click()
+            assert(splash:wait(2))
             splash:set_viewport_full()
-            return{splash:html(), splash:png()}
+            return{splash:png(), splash:html()}
         end
     '''
-    
+
     def start_requests(self):
         yield SplashRequest(url='https://www.adamchoi.co.uk/overs/detailed', callback=self.parse,
-                            endpoint='execute', args={'lua_source':self.scriipt})
+                            endpoint='execute', args={'lua_source':self.script})
 
 
     def parse(self, response):
